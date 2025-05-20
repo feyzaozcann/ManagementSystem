fb110f0326961053c308ed726b62884de7e516ce
feyza <feyzaozcan57@gmail.com>
2025-05-20 18:50:40 +0300

    Initial commit

---
+++ BaseLibrary/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace BaseLibrary.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        [JsonIgnore] //One to many relationship
+        public List<Employee>? Employees { get; set; }
+
+
+    }
+}
